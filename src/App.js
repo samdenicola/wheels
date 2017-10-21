451f002307eb76943c5064996e7c97cff32e478d
Sam DeNicola <samdenicola@teselagen.com>
2017-10-21 11:28:52 -0500

    with a map

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
 import fire from './fire';
+import mapboxgl from 'mapbox-gl';
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2FtZGVuaWNvbGEiLCJhIjoiY2o5MDA3a3phMDZ6NzJ4cDRva2FzbGlhdiJ9.IJ4t6Jdf88BVZx2-DFT2uA';
+
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { messages: [] }; // <- set up react state
+    this.state = { 
+      messages: [],
+      lng: 5,
+      lat: 34,
+      zoom: 1.5
+     }; // <- set up react state
   }
   componentWillMount(){
     /* Create reference to messages in Firebase Database */
@@ -16,6 +25,26 @@ class App extends Component {
       this.setState({ messages: [message].concat(this.state.messages) });
     })
   }
+  componentDidMount() {
+    const { lng, lat, zoom } = this.state;
+
+    const map = new mapboxgl.Map({
+      container: this.mapContainer,
+      style: 'mapbox://styles/mapbox/streets-v9',
+      center: [lng, lat],
+      zoom
+    });
+    map.on('move', () => {
+      const { lng, lat } = map.getCenter();
+
+      this.setState({
+        lng: lng.toFixed(4),
+        lat: lat.toFixed(4),
+        zoom: map.getZoom().toFixed(2)
+      });
+    });
+  }
+
   addMessage(e){
     e.preventDefault(); // <- prevent form submit from reloading the page
     /* Send the message to Firebase */
@@ -23,6 +52,8 @@ class App extends Component {
     this.inputEl.value = ''; // <- clear the input
   }
   render() {
+    const { lng, lat, zoom } = this.state;
+
     return (
       <div className="app">
         <div className="app-header">
@@ -33,6 +64,15 @@ class App extends Component {
           <p className="app-intro">
             a community for those on wheels
           </p>
+          <div style={{width: '500px', height: '500px', display: 'flex',
+    flexDirection: 'column', alignSelf: "center"}}>
+            <div className="">
+              <div>{`Longitude: ${lng} Latitude: ${lat} Zoom: ${zoom}`}</div>
+            </div>
+            <div ref={el => this.mapContainer = el} style={{width: '500px', height: '500px', display: 'flex',
+    flexDirection: 'column'}} className={'map-container'}/>
+          </div>
+
           <form onSubmit={this.addMessage.bind(this)}>
             <input type="text" ref={ el => this.inputEl = el }/>
             <input type="submit"/>
